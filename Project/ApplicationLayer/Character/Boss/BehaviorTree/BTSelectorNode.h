3aa4041cd7b786278cedadece7aad9c71cdace05
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-05-28 00:11:36 +0900

    Fix BT selector/sequence unresolved externals with inline defs

--- Project/ApplicationLayer/Character/Boss/BehaviorTree/BTSelectorNode.h
+++ Project/ApplicationLayer/Character/Boss/BehaviorTree/BTSelectorNode.h
@@ -7,10 +7,25 @@
 class BTSelectorNode : public IBTNode
 {
 public:
-    void AddChild(std::unique_ptr<IBTNode> child);
-    BTNodeResult Tick(float deltaTime) override;
+    void AddChild(std::unique_ptr<IBTNode> child)
+    {
+        children_.push_back(std::move(child));
+    }
+
+    BTNodeResult Tick(float deltaTime) override
+    {
+        for (auto& child : children_)
+        {
+            const BTNodeResult result = child->Tick(deltaTime);
+            if (result == BTNodeResult::Success || result == BTNodeResult::Running)
+            {
+                return result;
+            }
+        }
+
+        return BTNodeResult::Failure;
+    }
 
 private:
     std::vector<std::unique_ptr<IBTNode>> children_;
 };
-
