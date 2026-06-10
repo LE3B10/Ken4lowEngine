de567f10dff2508da3846a552b7a743237996a38
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-06-10 20:26:41 +0900

    Declare boss mesh particle helpers

--- Project/ApplicationLayer/Character/Boss/Attacks/BossAttackEffects.h
+++ Project/ApplicationLayer/Character/Boss/Attacks/BossAttackEffects.h
@@ -11,4 +11,6 @@ namespace Ken4lowEngine
 namespace BossAttackEffects
 {
 	void EmitGuardianHitEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
+	void EmitGuardianMeshDebrisEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
+	void EmitGuardianAttackPresenceEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
 }
