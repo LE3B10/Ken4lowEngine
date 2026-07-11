cf8394a424855d416031ce337c9b70e2cb7ae441
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-11 13:58:20 +0900

    Add centralized editor panel identifiers

---
+++ Project/Engine/Editor/EditorPanelIds.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Ken4lowEngine::EditorPanelIds
+{
+	inline constexpr const char* Toolbar = "Toolbar";
+	inline constexpr const char* PlaceActors = "Place Actors";
+	inline constexpr const char* MainViewport = "Main Viewport";
+	inline constexpr const char* WorldOutliner = "World Outliner";
+	inline constexpr const char* Details = "Details";
+	inline constexpr const char* ContentBrowser = "Content Browser";
+	inline constexpr const char* OutputLog = "Output Log";
+	inline constexpr const char* Scene = "Scene";
+
+	inline constexpr const char* Parameters = "Parameters";
+	inline constexpr const char* Display = "Display";
+	inline constexpr const char* PostEffectSettings = "Post Effect Settings";
+	inline constexpr const char* LightEditor = "Light Editor";
+	inline constexpr const char* JsonAssetManager = "Json Asset Manager";
+
+	inline constexpr const char* GameDebug = "Game Debug";
+	inline constexpr const char* CollisionDebug = "Collision Debug";
+	inline constexpr const char* CullingDebug = "Culling Debug";
+	inline constexpr const char* PhysicsWorldDebug = "PhysicsWorld Debug";
+	inline constexpr const char* GpuParticleEditor = "GPU Particle Editor";
+} // namespace Ken4lowEngine::EditorPanelIds
