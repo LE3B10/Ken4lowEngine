0296407b8b809afefc522c1cec7a481c1676ed8d
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-11 22:31:19 +0900

    Track editor play sessions

--- Project/Engine/Scene/Management/SceneManager.h
+++ Project/Engine/Scene/Management/SceneManager.h
@@ -9,9 +9,6 @@
 
 namespace Ken4lowEngine
 {
-	/// -------------------------------------------------------------
-	/// シーンの生成・切り替え・更新・描画を管理するクラス
-	/// -------------------------------------------------------------
 	class SceneManager
 	{
 	public:
@@ -27,42 +24,22 @@ namespace Ken4lowEngine
 		void DrawImGui();
 		void Finalize();
 
-		// 次のシーンの所有権をSceneManagerへ移す。
-		void SetNextScene(std::unique_ptr<BaseScene> nextScene)
-		{
-			nextScene_ = std::move(nextScene);
-		}
-
-		// ゲーム固有のSceneFactoryをSceneManagerへ登録する。
-		void SetAbstractSceneFactory(std::unique_ptr<AbstractSceneFactory> sceneFactory)
-		{
-			sceneFactory_ = std::move(sceneFactory);
-		}
-
-		// ゲーム固有のシーン遷移演出をSceneManagerへ登録する。
-		void SetSceneTransition(std::unique_ptr<ISceneTransition> sceneTransition)
-		{
-			sceneTransition_ = std::move(sceneTransition);
-		}
-
+		void SetNextScene(std::unique_ptr<BaseScene> nextScene) { nextScene_ = std::move(nextScene); }
+		void SetAbstractSceneFactory(std::unique_ptr<AbstractSceneFactory> sceneFactory) { sceneFactory_ = std::move(sceneFactory); }
+		void SetSceneTransition(std::unique_ptr<ISceneTransition> sceneTransition) { sceneTransition_ = std::move(sceneTransition); }
 		void ChangeScene(const std::string& sceneName);
 
-		[[nodiscard]] bool IsTransitioning() const
-		{
-			return isTransitioning_ || (sceneTransition_ && sceneTransition_->IsBusy());
-		}
-
+		[[nodiscard]] bool IsTransitioning() const { return isTransitioning_ || (sceneTransition_ && sceneTransition_->IsBusy()); }
 		[[nodiscard]] BaseScene* GetCurrentScene() { return scene_.get(); }
 		[[nodiscard]] const BaseScene* GetCurrentScene() const { return scene_.get(); }
-
-		// Editorは所有せず、現在登録されている遷移演出を参照する。
 		[[nodiscard]] ISceneTransition* GetSceneTransition() { return sceneTransition_.get(); }
 		[[nodiscard]] const ISceneTransition* GetSceneTransition() const { return sceneTransition_.get(); }
 
 	private:
 		void ApplyNextScene();
+		void UpdateEditorPlaySession();
+		void RefreshEditorVisualState(float deltaTime);
 
-	private:
 		std::unique_ptr<BaseScene> scene_;
 		std::unique_ptr<BaseScene> nextScene_;
 		std::unique_ptr<AbstractSceneFactory> sceneFactory_;
@@ -71,15 +48,13 @@ namespace Ken4lowEngine
 		bool isTransitioning_ = false;
 		bool sceneSwapped_ = false;
 		bool pendingCrack_ = false;
-
 		bool hasQueuedChange_ = false;
 		std::string queuedSceneName_;
-
 		int coverHoldFrames_ = 4;
 		int coverHoldCounter_ = 0;
 		int uncoverDelayFrames_ = 3;
 		int uncoverDelayCounter_ = 0;
-
 		bool unloadRequested_ = false;
+		bool editorPlaySessionActive_ = false; // Pauseでは維持し、Stop時だけEdit Worldを復元する。
 	};
-}
+} // namespace Ken4lowEngine
