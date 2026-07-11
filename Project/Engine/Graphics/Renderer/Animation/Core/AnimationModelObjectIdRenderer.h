72989046671b7f9d3c189b52f3e1a0bd7d79c172
け、ん？ <knt411sirokuro@gmail.com>
2026-07-11 19:43:43 +0900

    AnimationModelObjectIdRenderer.h の inline 削除とレイアウト更新
    AnimationModelObjectIdRenderer.h の DrawAnimationModelObjectId 関数から inline 修飾子を削除し、通常の関数宣言に変更しました。
    ken4low_editor_layout_v2.ini の DockNode (ID=0x0000000A) の Selected プロパティを更新しました。

--- Project/Engine/Graphics/Renderer/Animation/Core/AnimationModelObjectIdRenderer.h
+++ Project/Engine/Graphics/Renderer/Animation/Core/AnimationModelObjectIdRenderer.h
@@ -9,6 +9,6 @@ namespace Ken4lowEngine
 	/// <summary>
 	/// AnimationModelが公開するMeshとWorldTransformを使い、Editor Object-ID Passへ形状を描画します。
 	/// </summary>
-	inline void DrawAnimationModelObjectId(AnimationModel& animationModel, uint32_t objectId);
+	void DrawAnimationModelObjectId(AnimationModel& animationModel, uint32_t objectId);
 	
 } // namespace Ken4lowEngine
