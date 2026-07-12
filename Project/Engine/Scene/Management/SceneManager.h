db3f62e85dfba1510b73a9d67c6eb9615ed0b169
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-12 14:35:08 +0900

    feat: resolve scenes from scene definition registry

--- Project/Engine/Scene/Management/SceneManager.h
+++ Project/Engine/Scene/Management/SceneManager.h
@@ -3,6 +3,7 @@
 #include <BaseScene.h>
 #include "AbstractSceneFactory.h"
 #include "ISceneTransition.h"
+#include "SceneDefinitionRegistry.h"
 
 #include <memory>
 #include <string>
@@ -28,7 +29,12 @@ namespace Ken4lowEngine
 		void SetNextScene(std::unique_ptr<BaseScene> nextScene) { nextScene_ = std::move(nextScene); }
 		void SetAbstractSceneFactory(std::unique_ptr<AbstractSceneFactory> sceneFactory) { sceneFactory_ = std::move(sceneFactory); }
 		void SetSceneTransition(std::unique_ptr<ISceneTransition> sceneTransition) { sceneTransition_ = std::move(sceneTransition); }
-		void ChangeScene(const std::string& sceneName);
+		void ChangeScene(const std::string& sceneId);
+
+		bool LoadSceneDefinitions(const std::string& registryPath);
+		[[nodiscard]] std::string GetStartupSceneName(bool debugBuild) const;
+		[[nodiscard]] const SceneDefinition* FindSceneDefinition(const std::string& sceneId) const;
+		[[nodiscard]] const SceneDefinition& GetCurrentSceneDefinition() const { return currentSceneDefinition_; }
 
 		[[nodiscard]] bool IsTransitioning() const { return isTransitioning_ || (sceneTransition_ && sceneTransition_->IsBusy()); }
 		[[nodiscard]] bool IsPlayInEditorActive() const { return editorPlaySessionActive_; }
@@ -40,11 +46,15 @@ namespace Ken4lowEngine
 	private:
 		void ApplyNextScene();
 		void RefreshEditorVisualState(float deltaTime);
+		SceneDefinition ResolveSceneDefinition(const std::string& sceneId) const;
 
 		std::unique_ptr<BaseScene> scene_;
 		std::unique_ptr<BaseScene> nextScene_;
 		std::unique_ptr<AbstractSceneFactory> sceneFactory_;
 		std::unique_ptr<ISceneTransition> sceneTransition_;
+		SceneDefinitionRegistry sceneDefinitionRegistry_;
+		SceneDefinition currentSceneDefinition_{};
+		SceneDefinition nextSceneDefinition_{};
 
 		bool isTransitioning_ = false;
 		bool sceneSwapped_ = false;
