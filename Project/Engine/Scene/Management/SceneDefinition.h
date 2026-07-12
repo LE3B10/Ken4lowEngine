6f9ef5f3f72b804c1f2651adf8843fcb6d81607a
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-12 14:33:56 +0900

    feat: add data-driven scene definition

---
+++ Project/Engine/Scene/Management/SceneDefinition.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <json.hpp>
+
+#include <string>
+
+namespace Ken4lowEngine
+{
+	/// <summary>Scene遷移や使用Levelなど、C++ Sceneへ適用するデータ定義です。</summary>
+	struct SceneDefinition
+	{
+		struct TransitionSettings
+		{
+			std::string type = "Fade";
+			float duration = 1.0f;
+		};
+
+		std::string id;
+		std::string className;
+		std::string levelPath;
+		std::string gameMode;
+		std::string playerActor;
+		std::string uiLayout;
+		std::string bgmPath;
+		std::string nextScene;
+		std::string retryScene;
+		bool editorOnly = false;
+		TransitionSettings transition{};
+		nlohmann::json parameters = nlohmann::json::object();
+
+		[[nodiscard]] bool IsValid() const
+		{
+			return !id.empty() && !className.empty(); // Scene IDと生成するC++ Classは必須項目として扱う。
+		}
+	};
+} // namespace Ken4lowEngine
