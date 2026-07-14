15eef6aece9510ed28b8c019058e0b7f5051af81
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-14 16:36:52 +0900

    ColliderComponentの衝突通知をActorへ配送

--- Project/Engine/Scene/Actor/Components/ColliderComponent.cpp
+++ Project/Engine/Scene/Actor/Components/ColliderComponent.cpp
@@ -3,6 +3,7 @@
 #include <SceneComponent.h>
 
 #include <algorithm>
+#include <utility>
 
 #ifdef USE_IMGUI
 #include <imgui.h>
@@ -10,6 +11,65 @@
 
 namespace Ken4lowEngine
 {
+	class ColliderComponent::ForwardingCollider final : public Collider
+	{
+	public:
+		explicit ForwardingCollider(ColliderComponent* owner) : owner_(owner) {}
+
+		void OnCollision(Collider* other) override
+		{
+			if (owner_) owner_->DispatchCollision(other);
+		}
+
+		void OnCollisionEnter(Collider* other) override
+		{
+			if (owner_) owner_->DispatchCollisionEnter(other);
+		}
+
+		void OnCollisionStay(Collider* other) override
+		{
+			if (owner_) owner_->DispatchCollisionStay(other);
+		}
+
+		void OnCollisionExit(Collider* other) override
+		{
+			if (owner_) owner_->DispatchCollisionExit(other);
+		}
+
+		void OnCollisionEnter(const CollisionHit& hit) override
+		{
+			if (owner_) owner_->DispatchCollisionEnter(hit);
+		}
+
+		void OnCollisionStay(const CollisionHit& hit) override
+		{
+			if (owner_) owner_->DispatchCollisionStay(hit);
+		}
+
+		void OnCollisionExit(const CollisionHit& hit) override
+		{
+			if (owner_) owner_->DispatchCollisionExit(hit);
+		}
+
+		void OnOverlapBegin(const CollisionHit& hit) override
+		{
+			if (owner_) owner_->DispatchOverlapBegin(hit);
+		}
+
+		void OnOverlapStay(const CollisionHit& hit) override
+		{
+			if (owner_) owner_->DispatchOverlapStay(hit);
+		}
+
+		void OnOverlapEnd(const CollisionHit& hit) override
+		{
+			if (owner_) owner_->DispatchOverlapEnd(hit);
+		}
+
+	private:
+		ColliderComponent* owner_ = nullptr;
+	};
+
 	namespace
 	{
 		/// <summary>
@@ -63,18 +123,18 @@ namespace Ken4lowEngine
 
 			return ECollisionShapeType::None;
 		}
-
 	}
 
 	void ColliderComponent::Initialize()
 	{
 		SceneComponent::Initialize(); // 親子関係を考慮したWorldTransformを初期計算する
 
-		collider_ = std::make_unique<Collider>();	   // Colliderを生成する
-		collider_->Initialize();					   // Colliderの初期化処理を行う
-		collider_->SetDebugName(GetName());			   // Colliderのデバッグ名を設定する
-		collider_->SetCollisionLayer(collisionLayer_); // Colliderの衝突レイヤーを設定する
-		collider_->SetOwner(this);					   // Colliderの所有者を設定する
+		collider_ = std::make_unique<ForwardingCollider>(this); // Collision通知をComponentへ戻すColliderを生成する
+		collider_->Initialize();
+		collider_->SetDebugName(GetName());
+		collider_->SetCollisionLayer(collisionLayer_);
+		collider_->SetTrigger(isTrigger_);
+		if (Actor* owner = GetOwner()) collider_->SetOwner(owner); // Legacy CollisionManagerからも実際のActorをOwnerとして取得できるようにする
 
 		SyncFromSceneTransform();
 	}
@@ -130,7 +190,7 @@ namespace Ken4lowEngine
 		}
 
 		root->RefreshWorldTransform(); // RootComponentのWorldTransformを更新する
-		SyncFromSceneTransform();	   // SceneComponentのTransformをColliderへ再同期する
+		SyncFromSceneTransform();      // SceneComponentのTransformをColliderへ再同期する
 	}
 
 	void ColliderComponent::DrawImGui()
@@ -138,23 +198,22 @@ namespace Ken4lowEngine
 		SceneComponent::DrawImGui(); // ColliderのTransform情報を確認・編集できるようにする。
 
 #ifdef USE_IMGUI
-
 		ImGui::SeparatorText("Collider Component");
 		ComponentPropertyUtility::DrawImGui(CreateProperties());
-
 #endif
 	}
 
 	void ColliderComponent::Finalize()
 	{
-		// Colliderの破棄はActorがComponentを所有コンテナから削除する際にunique_ptrへ任せる。
+		ClearCollisionCallbacks(); // Actor再構築後に古い所有者へ通知しないよう外部参照を解除する
+		collider_.reset();
 	}
 
 	void ColliderComponent::ToJson(nlohmann::json& outJson) const
 	{
 		SceneComponent::ToJson(outJson); // SceneComponent共通情報をJSONへ保存する
 
-		outJson["Class"] = GetClassTypeName();								// ColliderComponentのクラス種別をJSONへ保存する
+		outJson["Class"] = GetClassTypeName();
 		ComponentPropertyUtility::ToJson(const_cast<ColliderComponent*>(this)->CreateProperties(), outJson);
 	}
 
@@ -207,6 +266,78 @@ namespace Ken4lowEngine
 		SetCollisionLayer(static_cast<uint32_t>(layer));
 	}
 
+	void ColliderComponent::ClearCollisionCallbacks()
+	{
+		onCollision_ = {};
+		onCollisionEnter_ = {};
+		onCollisionStay_ = {};
+		onCollisionExit_ = {};
+		onCollisionEnterHit_ = {};
+		onCollisionStayHit_ = {};
+		onCollisionExitHit_ = {};
+		onOverlapBegin_ = {};
+		onOverlapStay_ = {};
+		onOverlapEnd_ = {};
+	}
+
+	void ColliderComponent::DispatchCollision(Collider* other)
+	{
+		if (onCollision_) onCollision_(other);
+	}
+
+	void ColliderComponent::DispatchCollisionEnter(Collider* other)
+	{
+		if (onCollisionEnter_) onCollisionEnter_(other);
+		else DispatchCollision(other);
+	}
+
+	void ColliderComponent::DispatchCollisionStay(Collider* other)
+	{
+		if (onCollisionStay_) onCollisionStay_(other);
+		else DispatchCollision(other);
+	}
+
+	void ColliderComponent::DispatchCollisionExit(Collider* other)
+	{
+		if (onCollisionExit_) onCollisionExit_(other);
+	}
+
+	void ColliderComponent::DispatchCollisionEnter(const CollisionHit& hit)
+	{
+		if (onCollisionEnterHit_) onCollisionEnterHit_(hit);
+		else DispatchCollisionEnter(hit.other);
+	}
+
+	void ColliderComponent::DispatchCollisionStay(const CollisionHit& hit)
+	{
+		if (onCollisionStayHit_) onCollisionStayHit_(hit);
+		else DispatchCollisionStay(hit.other);
+	}
+
+	void ColliderComponent::DispatchCollisionExit(const CollisionHit& hit)
+	{
+		if (onCollisionExitHit_) onCollisionExitHit_(hit);
+		else DispatchCollisionExit(hit.other);
+	}
+
+	void ColliderComponent::DispatchOverlapBegin(const CollisionHit& hit)
+	{
+		if (onOverlapBegin_) onOverlapBegin_(hit);
+		else DispatchCollisionEnter(hit);
+	}
+
+	void ColliderComponent::DispatchOverlapStay(const CollisionHit& hit)
+	{
+		if (onOverlapStay_) onOverlapStay_(hit);
+		else DispatchCollisionStay(hit);
+	}
+
+	void ColliderComponent::DispatchOverlapEnd(const CollisionHit& hit)
+	{
+		if (onOverlapEnd_) onOverlapEnd_(hit);
+		else DispatchCollisionExit(hit);
+	}
+
 	void ColliderComponent::SyncFromSceneTransform()
 	{
 		if (!collider_)
@@ -222,11 +353,10 @@ namespace Ken4lowEngine
 			{
 				Sphere sphere{};
 				sphere.center = GetWorldPosition(); // SceneComponentのWorld位置をColliderの中心に反映する
-				sphere.radius = radius_;			// 設定された半径をColliderに反映する
-				collider_->SetSphere(sphere);		// ColliderにSphere情報を設定する
+				sphere.radius = radius_;            // 設定された半径をColliderに反映する
+				collider_->SetSphere(sphere);        // ColliderにSphere情報を設定する
 				break;
 			}
-			break;
 		case Ken4lowEngine::ECollisionShapeType::AABB:
 			{
 				Vector3 center = GetWorldPosition(); // SceneComponentのWorld位置をColliderの中心に反映する
@@ -235,8 +365,8 @@ namespace Ken4lowEngine
 				aabb.min = center - halfSize_;
 				aabb.max = center + halfSize_;
 				collider_->SetAABB(aabb);
+				break;
 			}
-			break;
 		case Ken4lowEngine::ECollisionShapeType::OBB:
 			{
 				collider_->SetCenterPosition(GetWorldPosition()); // SceneComponentのWorld位置をColliderの中心に反映する
@@ -244,7 +374,6 @@ namespace Ken4lowEngine
 				collider_->SetOrientation(GetWorldRotation()); // SceneComponentのWorld回転をColliderの回転に反映する
 				break;
 			}
-			break;
 		case Ken4lowEngine::ECollisionShapeType::Capsule:
 			break;
 		case Ken4lowEngine::ECollisionShapeType::Segment:
@@ -264,4 +393,4 @@ namespace Ken4lowEngine
 			{ "CollisionLayer", "Collision Layer", ComponentPropertyType::Int, [this]() -> ComponentPropertyValue { return static_cast<int>(collisionLayer_); }, [this](const ComponentPropertyValue& value) { if (const auto* typedValue = std::get_if<int>(&value)) { SetCollisionLayer(static_cast<uint32_t>(std::max(*typedValue, 0))); } }, 0.0f, 31.0f, 1.0f, true }
 		};
 	}
-}
+} // namespace Ken4lowEngine
