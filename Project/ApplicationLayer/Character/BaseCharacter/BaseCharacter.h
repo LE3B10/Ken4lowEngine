586f2b71623cf279912f1ea5bf564f4e3578e5f3
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-14 18:22:28 +0900

    BaseCharacter互換名前空間を復元

--- Project/ApplicationLayer/Character/BaseCharacter/BaseCharacter.h
+++ Project/ApplicationLayer/Character/BaseCharacter/BaseCharacter.h
@@ -2,5 +2,8 @@
 
 #include <Scene/Actor/Character/HumanoidCharacterActor.h>
 
+// 旧ApplicationLayerコードがBaseCharacter.h経由で利用していた名前空間エイリアスを維持する。
+namespace K4E = ::Ken4lowEngine;
+
 /// 旧includeパスを壊さず、実体は共通Actor/Component構成へ一本化する。
 using BaseCharacter = Ken4lowEngine::HumanoidCharacterActor;
