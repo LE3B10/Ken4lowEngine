ee62b3b556e5099fd78f898414a7ccce03da35e8
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-14 16:55:56 +0900

    人型BossのCollider設定を共通Componentへ反映

--- Project/ApplicationLayer/Character/Boss/BaseTypes/HumanoidBossBase.h
+++ Project/ApplicationLayer/Character/Boss/BaseTypes/HumanoidBossBase.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Core/BossBase.h"
+#include "CollisionPreset.h"
 
 /// 人型ボスの部位所有はHumanoidVisualComponentへ統一し、この型はBossBaseへの薄い構成指定だけを残す。
 class HumanoidBossBase : public BossBase
@@ -11,6 +12,8 @@ public:
 	void BuildBossParts() override
 	{
 		BaseCharacter::Initialize(); // 個別Object3D生成を行わず、HumanoidVisualComponentの定義から一括生成する。
+		ApplyCollisionPreset(*this, ECollisionPresetId::Boss); // Component生成前に行われた旧Preset設定を実体Colliderへ反映し直す。
+		if (auto* collider = GetColliderComponent()) collider->SetHalfSize({ 1.25f, 1.75f, 1.25f });
 	}
 
 	/// 具体的なボス初期値は派生Bossが設定するため、共通側では追加処理を持たない。
