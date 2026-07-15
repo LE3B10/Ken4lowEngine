4e16bad9c351d9b660de5572994db3aa4a4f51eb
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-15 15:56:33 +0900

    FenceManagerにFrameResource用Fence値APIを追加

--- Project/Engine/Graphics/Device/Synchronization/DX12FenceManager.h
+++ Project/Engine/Graphics/Device/Synchronization/DX12FenceManager.h
@@ -13,50 +13,28 @@ class DX12FenceManager
 {
 public: /// ---------- メンバ関数 ---------- ///
 
-	/// <summary>
-	/// フェンスの初期化処理を行います。<br/>
-	/// ・ID3D12Device::CreateFence() でフェンスを生成<br/>
-	/// ・CreateEvent() で GPU 完了待ち用のイベントオブジェクトを作成<br/>
-	/// します。失敗した場合は assert で停止します。
-	/// </summary>
-	/// <param name="device">フェンスを生成するための ID3D12Device。</param>
 	void Initialize(ID3D12Device* device);
-
-	/// <summary>
-	/// フェンスの終了処理を行います。<br/>
-	/// ・イベントハンドルが有効なら CloseHandle() でクローズ<br/>
-	/// ・ID3D12Fence を Reset() で解放<br/>
-	/// します。アプリ終了時や DirectX 解放時に呼び出します。
-	/// </summary>
 	void Finalize();
 
-	/// <summary>
-	/// GPU にフェンスシグナルを送信します。<br/>
-	/// ・内部カウンタ fenceValue_ をインクリメントし、<br/>
-	/// ・ID3D12CommandQueue::Signal() を呼び出して GPU キューにシグナルを送ります。<br/>
-	/// この値を基準に Wait() 側で完了待ちを行います。
-	/// </summary>
-	/// <param name="commandQueue">シグナルを送る対象のコマンドキュー。</param>
+	/// 従来互換: 新しいFence値を発行してQueueへSignalする。
 	void Signal(ID3D12CommandQueue* commandQueue);
 
-	/// <summary>
-	/// GPU の処理が完了するまで CPU を待機させます。<br/>
-	/// ・fence_->GetCompletedValue() が fenceValue_ 未満なら、<br/>
-	/// 　SetEventOnCompletion() でイベントとフェンス値を関連付けて、<br/>
-	/// 　WaitForSingleObject() で完了までブロックします。<br/>
-	/// それ以外の場合は即座に復帰します。
-	/// </summary>
+	/// 発行したFence値を返し、FrameResourceごとの完了値として保存できるようにする。
+	UINT64 SignalAndGetValue(ID3D12CommandQueue* commandQueue);
+
+	/// 従来互換: 最後に発行したFence値まで待機する。
 	void Wait();
+
+	/// 指定Fence値まで必要な場合だけ待機する。
+	void WaitForValue(UINT64 fenceValue);
+
+	UINT64 GetCompletedValue() const;
+	UINT64 GetCurrentValue() const { return fenceValue_; }
 
 private: /// ---------- メンバ変数 ---------- ///
 
-	// GPU と CPU の同期に使用するフェンスオブジェクト
 	Microsoft::WRL::ComPtr<ID3D12Fence> fence_;
-
-	// フェンス完了通知用のイベントハンドル
 	HANDLE fenceEvent_ = nullptr;
-
-	// フェンスの現在値を管理するカウンタ
 	UINT64 fenceValue_ = 0;
 };
 
