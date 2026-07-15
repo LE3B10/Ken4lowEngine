8fdb77461a34f076fd5e7cde1781178cc1c5c290
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-15 22:32:01 +0900

    Stage1Tutorial入力制限のPlayer移行ブリッジを追加

---
+++ Project/ApplicationLayer/Character/Player/Migration/PlayerTutorialRestrictionBridge.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <functional>
+
+/// P11移行中だけStage1Tutorialの入力許可状態を新PlayerInputComponentへ渡すための一時ブリッジ。
+class PlayerTutorialRestrictionBridge
+{
+public:
+	struct State
+	{
+		bool enabled = false;
+		bool allowMove = true;
+		bool allowShoot = true;
+		bool allowReload = true;
+	};
+
+	using Provider = std::function<State()>;
+
+	static void SetProvider(Provider provider) { provider_ = std::move(provider); }
+	static void ClearProvider() { provider_ = {}; }
+	static State GetState() { return provider_ ? provider_() : State{}; }
+
+private:
+	inline static Provider provider_{};
+};
