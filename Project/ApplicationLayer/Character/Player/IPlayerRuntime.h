56333706ad4911b64ca9120493b9f541c1cc03ee
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-15 23:23:19 +0900

    Player Runtime境界をGamePlay依存へ拡張

--- Project/ApplicationLayer/Character/Player/IPlayerRuntime.h
+++ Project/ApplicationLayer/Character/Player/IPlayerRuntime.h
@@ -1,6 +1,14 @@
 #pragma once
 
-/// GamePlay側が巨大な具象Playerへ直接依存せず、段階移行中も共通して参照する最小Runtime境界。
+#include <Vector3.h>
+
+namespace Ken4lowEngine
+{
+	class Camera;
+	class Collider;
+}
+
+/// GamePlay側が巨大な具象Playerへ直接依存せず、PlayerActorを正本として扱うRuntime境界。
 class IPlayerRuntime
 {
 public:
@@ -11,4 +19,23 @@ public:
 	virtual bool IsGameOverReady() const = 0;
 	virtual bool ConsumeGameOverReady() = 0;
 	virtual bool IsDeathActive() const = 0;
+
+	virtual Ken4lowEngine::Vector3 GetWorldPosition() const = 0;
+	virtual Ken4lowEngine::Collider* GetCollisionPrimitive() = 0;
+	virtual const Ken4lowEngine::Collider* GetCollisionPrimitive() const = 0;
+	virtual Ken4lowEngine::Camera* GetCamera() const = 0;
+
+	virtual float ApplyRuntimeDamage(float amount) = 0;
+	virtual float HealRuntime(float amount) = 0;
+
+	virtual int AddReserveAmmo(int amount) = 0;
+	virtual int GetMagazineAmmo() const = 0;
+	virtual int GetMagazineCapacity() const = 0;
+	virtual int GetReserveAmmo() const = 0;
+	virtual int GetMaxReserveAmmo() const = 0;
+	virtual bool IsReloading() const = 0;
+	virtual float GetReloadTimer() const = 0;
+	virtual float GetReloadDuration() const = 0;
+
+	virtual void SetViewLookAngles(float pitch, float yaw) = 0;
 };
