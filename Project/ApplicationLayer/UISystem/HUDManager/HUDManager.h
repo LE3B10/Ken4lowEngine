29420372822451b1197e08767e67984ee7cc0716
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-19 14:20:21 +0900

    HUDManagerへ共通Objective表示を追加

--- Project/ApplicationLayer/UISystem/HUDManager/HUDManager.h
+++ Project/ApplicationLayer/UISystem/HUDManager/HUDManager.h
@@ -13,12 +13,31 @@
 #include "WeaponSlot.h"
 
 #include <memory>
+#include <string>
 
 class IPlayerRuntime;
+
+namespace Ken4lowEngine
+{
+	class Sprite;
+	class TextSpriteDrawer;
+}
 
 /// World/Tutorial/Boss表示とPlayerActorの武器HUDを管理する。
 class HUDManager
 {
+public:
+	struct StageObjectiveDisplayState
+	{
+		bool visible = false;
+		bool showProgress = false;
+		bool cleared = false;
+		bool failed = false;
+		std::string title;
+		std::string detail;
+		float normalizedProgress = 0.0f;
+	};
+
 public:
 	~HUDManager();
 
@@ -38,6 +57,7 @@ public:
 	void SetStage1TutorialItemMarker(int markerIndex, bool visible, const Ken4lowEngine::Vector2& screenPosition, int itemType);
 	void NotifyStage1ObjectiveGuideStarted();
 	void NotifyStage1BossAppeared();
+	void SetStageObjectiveDisplayState(const StageObjectiveDisplayState& state);
 	void SetBossGuide(const Ken4lowEngine::Vector3& playerPos, const Ken4lowEngine::Vector3& bossPos, const Ken4lowEngine::Vector3& cameraForward, bool bossBattleActive);
 	void NotifyBossIntroCompleted(const Ken4lowEngine::Vector3& bossPos);
 	void SetWeaponSlotVisibleSlotCount(int count);
@@ -77,6 +97,9 @@ private:
 	bool UpdateReloadCircleFromRuntime();
 	void UpdateWeaponSlotFromRuntime();
 	IPlayerRuntime* ResolvePlayerRuntime() const;
+	void InitializeStageObjectiveUI();
+	void UpdateStageObjectiveUI(float deltaTime);
+	void DrawStageObjectiveUI();
 
 	IPlayerRuntime* playerRuntime_ = nullptr;
 	bool runtimeWeaponHudVisible_ = true;
@@ -88,4 +111,13 @@ private:
 	std::unique_ptr<DamageIndicatorManager> damageIndicatorManager_;
 	BossHudUI bossHudUI_;
 	Stage1ObjectiveGuideUI stage1ObjectiveGuideUI_;
+
+	StageObjectiveDisplayState stageObjectiveDisplayState_{};
+	std::unique_ptr<Ken4lowEngine::Sprite> stageObjectiveBackSprite_;
+	std::unique_ptr<Ken4lowEngine::Sprite> stageObjectiveAccentSprite_;
+	std::unique_ptr<Ken4lowEngine::Sprite> stageObjectiveProgressBackSprite_;
+	std::unique_ptr<Ken4lowEngine::Sprite> stageObjectiveProgressFillSprite_;
+	std::unique_ptr<Ken4lowEngine::TextSpriteDrawer> stageObjectiveTextDrawer_;
+	bool stageObjectiveTextReady_ = false;
+	float stageObjectiveAlpha_ = 0.0f;
 };
