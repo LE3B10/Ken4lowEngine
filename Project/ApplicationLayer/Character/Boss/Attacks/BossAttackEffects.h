82e9b74e1b83bb8eb7836b13df1be2ab68feafd1
キクチケンタ <146914890+LE3B10@users.noreply.github.com>
2026-07-19 10:19:45 +0900

    ボス予兆用の軽量パーティクル入口を追加

--- Project/ApplicationLayer/Character/Boss/Attacks/BossAttackEffects.h
+++ Project/ApplicationLayer/Character/Boss/Attacks/BossAttackEffects.h
@@ -13,4 +13,5 @@ namespace BossAttackEffects
 	void EmitGuardianHitEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
 	void EmitGuardianMeshDebrisEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
 	void EmitGuardianAttackPresenceEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
+	void EmitGuardianTelegraphEffect(const char* emitterName, Ken4lowEngine::GpuParticleType particleType, const Ken4lowEngine::Vector3& position, uint32_t spawnCount, float spawnRadius, float lifetimeScale, float initialSpeedScale);
 }
